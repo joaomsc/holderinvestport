4ed5e638ed8400a75073a2b90eb041f670949185
joaomsc <juaum.dsb@gmail.com>
2020-06-07 00:48:55 -0300

    Authentication/Authority - plus - Company CRUD

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,7 @@
+import holderinvestport.CustomUserDetailsService
+import holderinvestport.UserPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
+    userPasswordEncoderListener(UserPasswordEncoderListener)
+    userDetailsService(CustomUserDetailsService)
 }
--- grails-app/init/holderinvestport/BootStrap.groovy
+++ grails-app/init/holderinvestport/BootStrap.groovy
@@ -3,6 +3,17 @@ package holderinvestport
 class BootStrap {
 
     def init = { servletContext ->
+        //Creating default roles
+        new Role(authority: 'ROLE_USER').save()
+        def admRole = new Role(authority: 'ROLE_ADMIN').save()
+
+        //Creating adm user
+        def admUser = new User(username: "admin@admin.com", password: "admin", fullname: "Admin").save()
+        UserRole.create admUser, admRole
+
+        //Populating some data
+        new Company(tradingName: 'nome fantasia', name: 'razão social', description: 'produtora de café').save()
+
     }
     def destroy = {
     }
